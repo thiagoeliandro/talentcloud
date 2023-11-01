8dc7e99566c35b00b6b1f95e2a2438d09a94ca9b
thiagoeliandro <thiagoeliandro@gmail.com>
2023-10-31 23:36:20 -0300

    Exercício de conexão

---
+++ Conexao js e html/script.js
@@ -0,0 +1,8 @@
+function imprimirMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+// Executar a função três vezes
+for (let i = 0; i < 3; i++) {
+    imprimirMensagem();
+}
---
+++ aula18-criar-elementos-no-dom/script.js
@@ -0,0 +1,42 @@
+const arrayPostagens = [
+  {
+    titulo: "Pop Vegan",
+    subtitulo: "Comida vegana para todos!",
+    data: "06/07/2022",
+    texto: "Restaurante em Consolação com comida por kilo no almoço e rodízio de pizzas à noite, tudo 100% vegano. Vale muito a pena conhecer :)"
+  },
+  {
+    titulo: "Make Hommus. Not War",
+    subtitulo: "Só delivery, para curtir em casa!",
+    data: "18/08/2022",
+    texto: "Neste restaurante não só pode, como é encorajado comer o antepasto como prato principal. Recomendamos os kibes e a kafta bonina."
+  },
+  {
+    titulo: "Churrascada do Mar",
+    subtitulo: "Melhor do que estar na praia!",
+    data: "30/08/2022",
+    texto: "Todos conhecemos e amamos um bom churrasco, mas o que você acha de experimentar um churrasco focado em frutos do mar? Nós gostamos, experimente e nos conte o que você achou!"
+  },
+  {
+    titulo: "Flamengo 2 x 3 Grêmio",
+    subtitulo: "Flamengo deixa escapar chance de assumir segunda colocação na tabela.",
+    data: "30/08/2022",
+    texto: "Todos conhecemos e amamos um bom churrasco, mas o que você acha de experimentar um churrasco focado em frutos do mar? Nós gostamos, experimente e nos conte o que você achou!"
+  },
+]
+
+const body = document.querySelector('main');
+
+for (let i = 0; i < arrayPostagens.length; i++) {
+
+  const article = document.createElement('article');
+
+  article.innerHTML = `
+  <h3>${arrayPostagens[i].titulo}</h3>
+  <p class="subtitulo">${arrayPostagens[i].subtitulo}</p>
+  <div class="data">${arrayPostagens[i].data}</div>
+  <p>${arrayPostagens[i].texto}</p>
+  `
+  body.appendChild(article);
+
+}
